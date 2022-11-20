1fcd53b463c8b6f3f69ce26e7cc290fa11dc9a88
渡邉将太 <shotawebcoding@gmail.com>
2022-11-20 17:58:39 +0900

    updated

--- indexedDB.js
+++ indexedDB.js
@@ -39,3 +39,89 @@ database.onsuccess = function (event) {
 database.onerror = function (event) { 
     console.log('データベースに接続できませんでした');
 }
+
+//フォームの内容をDBに登録する
+function regist() { 
+    //フォームの入力チェック。falseが返却されたら登録処理を中断
+    if (inputCheck() == false){
+        return;
+    }
+
+    //ラジオボタンの取得
+    let radio = document.getElementsByName("balance");
+    let balance;
+    for (let i = 0; i < radio.length; i++) {
+        if (radio[i].checked == true) {
+            balance = radio[i].value;
+            break;
+        }
+    }
+
+    //フォームに入力された値を取得
+    let date = document.getElementById("date").value;
+    let amount = document.getElementById("amount").value;
+    let memo = document.getElementById("memo").value;
+    let category = document.getElementById("category").value;
+    //ラジオボタンが収入を選択時はカテゴリを「収入」とする
+    if (balance == "収入") {
+        category = "収入";
+    }
+ 
+    //データベースにデータを登録する
+    insertData(balance, date, category, amount, memo);
+
+    //データの挿入
+    function insertData(balance, date, category, amount, memo) {
+        //一意のIDを現在の日時から作成
+        let uniqueID = new Date().getTime().toString();
+        console.log(uniqueID);
+        //DBに登録するための連想配列のデータを作成
+        let data = {
+            id: uniqueID,
+            balance: balance,
+            date: String(date),
+            category: category,
+            amount: amount,
+            memo: memo,
+        }
+
+        //データベースを開く
+        let database = indexedDB.open(dbName, dbVersion);
+
+        //データベースを開けなかった時の処理
+        database.onerror = function (event) {
+            console.log('データベースに接続できませんでした');
+        }
+
+        //データベースを開いたらデータの登録を実行
+        database.onsuccess = function (event) { 
+            let db = event.target.result;
+            //どのオブジェクトストアに対してトランザクション処理をするのかを設定
+            //変数storeNameに代入されているkakeiboStoreを対象にトランザクション処理を設定
+            let transaction = db.transaction(storeName, 'readwrite');
+
+
+           transaction.oncomplete = function (event) {
+               console.log("トランザクション完了");
+           }
+            
+            transaction.onerror = function (event) {
+                console.log("トランザクションエラー");
+            }
+
+            //オブジェクトストアを変数storeに代入してプログラムで操作できるようにしています。
+            let store = transaction.objectStore(storeName);
+            let addData = store.add(data);
+            
+            addData.onsuccess = function () {
+                console.log("データが登録できました");
+                alert("登録しました");
+            }
+        
+            addData.onerror = function () {
+                console.log("データが登録できませんでした");
+            }
+            db.close();
+        }
+    }
+}
